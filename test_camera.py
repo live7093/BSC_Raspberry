5ee03af4c4351cb32ef942ef9a939bbbf206e158
live7093 <schmiru3@students.zhaw.ch>
2026-03-21 15:02:02 +0100

    added resolution

--- test_camera.py
+++ test_camera.py
@@ -9,11 +9,14 @@ from libcamera import controls
 import time
 
 LENS_POSITION = 4.5  # 0.0 = infinity, 2.0 = ~50cm, 4.0 = ~25cm
+WIDTH, HEIGHT = 1920, 1080  # e.g. 1920x1080, 2592x1944 (full), 640x480 (fast)
+
 
 for idx in [0, 1]:
     print(f"Camera {idx}: capturing…")
     cam = Picamera2(idx)
     cam.configure(cam.create_still_configuration())
+    cam.configure(cam.create_still_configuration(main={"size": (WIDTH, HEIGHT)}))
     cam.start()
     cam.set_controls({"AfMode": controls.AfModeEnum.Manual, "LensPosition": LENS_POSITION})
     time.sleep(2)
